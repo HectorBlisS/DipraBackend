76a901a153bd7115357cbb94110f7d06db7b7f52
BlisS <contacto@fixter.org>
2017-07-17 07:24:47 -0500

    registro

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -15,3 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
 		model = User
 		fields = "__all__"
 
+	def create(self, validated_data):
+		user = User.objects.create_user(**validated_data)
+		return user
+

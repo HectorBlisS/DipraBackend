ed804efb16ebb5b73ddb5b50ada79915bba1f5dc
BlisS <contacto@fixter.org>
2017-08-10 14:45:20 -0500

    staticos6

--- dipra/settings.py
+++ dipra/settings.py
@@ -142,11 +142,11 @@ DATABASES['default'].update(db_from_env)
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
 # STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 

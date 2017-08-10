d228587bd91bee68c3a45a5961c66b38a5dfe60a
BlisS <contacto@fixter.org>
2017-08-10 14:44:22 -0500

    staticos5

--- dipra/settings.py
+++ dipra/settings.py
@@ -146,7 +146,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 

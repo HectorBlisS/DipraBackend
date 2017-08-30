3de98103c5db3d41657c2b7c26e663903c516c6f
BlisS <contacto@fixter.org>
2017-08-30 18:05:39 -0500

    filtro en polizas2

--- polizas/admin.py
+++ polizas/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Poliza, Cliente, Vehiculo, Recibo, Prospecto
 
 class PolizaAdmin(admin.ModelAdmin):
-	list_display = ['id','user','asesor','fecha_poliza','idcliente','genero']
-	list_filter = ['user','asesor', 'genero', 'fecha_poliza']
+	list_display = ['id','asesor','cliente','apertura']
+	list_filter = ['asesor','cliente','apertura']
 
 admin.site.register(Poliza, PolizaAdmin)
 admin.site.register(Cliente)

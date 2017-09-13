e77103507e3fd46b8f6d5ed97914c5fbd4a1c89f
Oswaldinho <oswalfut_96@hotmail.com>
2017-09-13 18:33:30 -0500

    adding all usertypes

--- accounts/models.py
+++ accounts/models.py
@@ -21,6 +21,9 @@ class Profile(models.Model):
 	asesorId = models.CharField(max_length=100, blank=True, null=True, unique=True)
 	clienteId = models.CharField(max_length=100, blank=True, null=True, unique=True)
 	aprobado = models.BooleanField(default=False)
+	jefe = models.BooleanField(default=True)
+	analista = models.BooleanField(default=True)
+	reclutador = models.BooleanField(default=True)
 
 
 	def __str__(self):
